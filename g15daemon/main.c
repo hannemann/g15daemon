8a6a1101bca2538d77d742d5946a966a56c988f1
Daniel Menelkir <menelkir@itroll.org>
2021-08-11 08:33:04 -0300

    modified:   g15daemon/main.c

--- g15daemon/main.c
+++ g15daemon/main.c
@@ -245,8 +245,8 @@ static void *lcd_draw_thread(void *lcdlist){
 			setLCDContrast(displaying->contrast_state);
 	//		if(displaying->masterlist->remote_keyhandler_sock==0) // only allow mled control if the macro recorder isnt running 
 				setLEDs(displaying->mkey_state); 
-			pthread_mutex_unlock(&g15lib_mutex);
-				displaying->state_changed = 0;
+	//		pthread_mutex_unlock(&g15lib_mutex);
+	//			displaying->state_changed = 0;
 		}
 		pthread_mutex_unlock(&lcdlist_mutex);
 	}
